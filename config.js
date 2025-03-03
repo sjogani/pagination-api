aa334aa1c4aab3ed2e3e0249df56318ac11ceca4
joganisoham <joganisoham@gmail.com>
2025-03-03 20:44:34 +0530

    Changing database to mysql

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();  // Load environment variables
 
-// Initialize Sequelize with PostgreSQL
+// Initialize Sequelize with MySQL
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false, // Disable logging SQL queries
+    dialect: 'mysql',
+    logging: false, // Disable SQL logs
 });
 
 module.exports = sequelize;
